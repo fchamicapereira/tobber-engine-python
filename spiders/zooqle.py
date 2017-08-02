2af49d8b694d99514cb08196443363e8706d4896
fchamicapereira <fchamicapereira@gmail.com>
2017-08-02 19:34:48 +0100

    just realized the data of the torrents doesn\'t match the website. Must fix it. Also added a href to the torrent site

--- spiders/zooqle.py
+++ spiders/zooqle.py
@@ -6,7 +6,7 @@ class Zooqle(scrapy.Spider):
     def start_requests(self):
         self.site = "https://zooqle.com"
 
-        tvshow = "rick and morty"
+        tvshow = "game of thrones"
         sortBySize = "&s=sz&v=t&sd=d"
 
         search = self.site + "/search?q=" + tvshow.replace(' ','%20') + sortBySize
@@ -19,6 +19,7 @@ class Zooqle(scrapy.Spider):
     def printTorrent(self,torrent):
         print '\n{'
         print '\t Title: ' + torrent['title']
+        print '\t href: ' + torrent['href']
         print '\t Torrent: ' + torrent['torrent']
         print '\t Magnet: ' + torrent['magnet']
         print '\t Size: ' + torrent['size']
@@ -27,20 +28,16 @@ class Zooqle(scrapy.Spider):
         print '}\n'
 
     def parse(self, response):
-        table = response.xpath('//table//tr//td')
+        table = response.xpath('//table//tr')
 
-        title   = table.xpath('//a[@class=" small"]')
-        peers   = table.xpath('//div[@class="progress prog trans70"]/@title')
-        magnet  = table.xpath('//a[@title="Magnet link"]/@href')
-        torrent = table.xpath('//a[@title="Generate .torrent"]/@href')
-        size    = table.xpath('//div[@class="progress prog prog-narrow trans90"]//div[@class="progress-bar prog-blue prog-l"]/text()')
+        title   = table.xpath('//td[@class="text-trunc text-nowrap "]//a[@class=" small"]')
+        href    = title.xpath('//td//@href')
+        peers   = table.xpath('//td//div[contains(@class,"progress prog")]/@title')
+        magnet  = table.xpath('//td//a[@title="Magnet link"]/@href')
+        torrent = table.xpath('//td//a[@title="Generate .torrent"]/@href')
+        size    = table.xpath('//td//div[@class="progress prog prog-narrow trans90"]//div[@class="progress-bar prog-blue prog-l"]/text()')
 
-        for i in range(0,20):
-
-            # peers' items come in format 'Seeders: X | Leechers: Y'
-            # this is extract the numbers from that string
-
-            peersList = [int(d) for d in peers[i].extract().split() if d.isdigit()]
+        for i in range(len(table)):
 
             # this is a hack
             # the title's text comes with <hl> tags that are
@@ -51,13 +48,17 @@ class Zooqle(scrapy.Spider):
             t = t.replace('<hl>','')
             t = t.replace('</hl>','')
 
+            # Seeders : X | Leechers: Y
+            peersTuple = peers[i].re(r'.*(\d+).*(\d+)')
+
             obj = {
-                'magnet': magnet[i].extract(),
-                'torrent': self.site + torrent[i].extract(),
-                'seeders': peersList[0],
-                'leechers': peersList[1],
-                'size': size[i].extract(),
-                'title': t
+                'magnet':   magnet[i].extract(),
+                'torrent':  self.site + torrent[i].extract(),
+                'seeders':  peersTuple[0],
+                'leechers': peersTuple[1],
+                'size':     size[i].extract(),
+                'title':    t,
+                'href' :    self.site + href[i].extract()
             }
 
             self.printTorrent(obj)
