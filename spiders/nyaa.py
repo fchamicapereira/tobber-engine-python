59a01479553711d720ec5752954175d63c9d653b
fchamicapereira <fchamicapereira@gmail.com>
2017-08-11 15:38:04 +0100

    Zooqle is now down (sad...). I\'m keeping the work made on it just in case, and added nyaa.patsu for anime

---
+++ spiders/nyaa.py
@@ -0,0 +1,59 @@
+import scrapy
+
+class Nyaa(scrapy.Spider):
+    name = "nyaa"
+
+    def start_requests(self):
+        self.site = "https://nyaa.pantsu.cat/"
+
+        tvshow = "steins;gate"
+        search = self.site + "/search?c=_&order=false&q=" + tvshow.replace(' ','+') + '&sort=4&userID=0'
+
+        #must return an iterable
+        #can be a list (like here) or a generator (use yield in that case)
+        return [scrapy.Request(url=search, callback=self.parse)]
+
+    # pretty printing
+    def printTorrent(self,torrent):
+        print '\n{'
+        print '\t Title: ' + torrent['title']
+        #print '\t href: ' + torrent['href']
+        print '\t Size: ' + torrent['size']
+        print '\t Category: ' + torrent['category']
+        print '\t Torrent: ' + torrent['torrent']
+        print '\t Magnet: ' + torrent['magnet']
+        #print '\t Seeders: ' + str(torrent['seeders'])
+        #print '\t Leechers: ' + str(torrent['leechers'])
+        print '}\n'
+
+    def parse(self, response):
+        #xPath rules
+        table   = response.xpath('//tbody[@id="torrentListResults"]//tr[contains(@class,"torrent-info")]')
+        title   = table.xpath('//td[@class="tr-name home-td"]//a/text()')
+        category   = table.xpath('//td[@class="tr-cat home-td"]//div//a/@title')
+        #href    = title.xpath('//td//@href')
+        #peers   = table.xpath('//td//div[contains(@class,"progress prog")]/@title')
+        links   = table.xpath('//td[contains(@class,"tr-links home-td")]')
+        magnet  = links.xpath('//a[@title="Magnet Link"]/@href')
+        torrent = links.xpath('//a[@title="Torrent file"]/@href')
+        size    = table.xpath('//td[contains(@class,"tr-size")]/text()')
+
+        for i in range(len(table)):
+
+            # Seeders : X | Leechers: Y
+            #peersTuple = peers[i].re(r'.*(\d+).*(\d+)')
+
+            obj = {
+                'magnet':   magnet[i].extract(),
+                'torrent':  torrent[i].extract(),
+                #'seeders':  peersTuple[0],
+                #'leechers': peersTuple[1],
+                'size':     size[i].extract().replace('\n',''),
+                'title':    title[i].extract().replace('\n',''),
+                'category':    category[i].extract().replace('\n','')
+                #'href' :    self.site + href[i].extract()
+            }
+
+            self.printTorrent(obj)
+
+            yield obj
