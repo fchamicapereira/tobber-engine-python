86b8453166cc846f7941145483b53dcc59f8bac3
fchamicapereira <fchamicapereira@gmail.com>
2017-08-21 13:18:04 +0100

    changed something i shouldnt have changed

--- tobber/pipelines/english_anime.py
+++ tobber/pipelines/english_anime.py
@@ -4,6 +4,11 @@ from scrapy.exceptions import DropItem
 # Dump raw anime
 class English_anime(object):
     def process_item(self, item, spider):
+        avoid = [
+            'non',
+            'raw',
+            'audio'
+        ]
 
         # only anime torrents have a category
         if 'category' in item and all(words not in item['category'].lower() for words in avoid):
